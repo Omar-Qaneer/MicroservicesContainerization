586977e00b75e735b3614303f5c953ebecd3478a
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2023-11-17 21:21:23 +0200

    Check book availability when purchasing

--- order-microservice/index.js
+++ order-microservice/index.js
@@ -5,6 +5,27 @@ const app = express();
 // Define a route to purchase a book
 app.post("/purchase/:itemNumber", async (req, res) => {
   const itemNumber = req.params.itemNumber;
+
+  // Check if the book is in stock
+  axios
+    .get(`http://localhost:3001/info/${itemNumber}`)
+    .then(async (response1) => {
+      const bookInfo = response1.data;
+      if (response1.status !== 200) {
+        res.status(400).send("Failed to get book info.");
+        return;
+      }
+      console.log(bookInfo[0].quantity);
+
+      if (bookInfo.quantity <= 0) {
+        res.status(400).send("Book out of stock.");
+        return;
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
   res.send("Purchase successful!");
 });
 
